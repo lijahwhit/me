0f053d1489d1b3d786232d10b9e33d653113c228
lijahwhit <elijah@whitti.com>
2023-06-06 13:30:45 -0400

    preset 1

--- billingAppFiles/js/main.js
+++ billingAppFiles/js/main.js
@@ -1,30 +1,27 @@
 document.getElementById("electric").value = getSavedValue("electric");
 document.getElementById("propane").value = getSavedValue("propane");
 document.getElementById("input0").value = getSavedValue("input0");
-document.getElementById("input1").value = getSavedValue("input1");    // set the value to this input
+document.getElementById("input1").value = getSavedValue("input1");
 document.getElementById("input2").value = getSavedValue("input2");
 document.getElementById("input3").value = getSavedValue("input3");
 document.getElementById("input4").value = getSavedValue("input4");
 document.getElementById("input5").value = getSavedValue("input5");
 document.getElementById("input6").value = getSavedValue("input6");
 document.getElementById("input7").value = getSavedValue("input7");
-document.getElementById("input7").value = getSavedValue("input8");
-document.getElementById("input7").value = getSavedValue("input9");
-document.getElementById("input7").value = getSavedValue("input10");
+document.getElementById("input8").value = getSavedValue("input8");
+document.getElementById("input9").value = getSavedValue("input9");
+document.getElementById("input10").value = getSavedValue("input10");
 getCcpf();
-/* Here you can add more inputs to set value. if it's saved */
 
-//Save the value function - save it to localStorage as (ID, VALUE)
 function saveValue(e){
-    var id = e.id;  // get the sender's id to save it . 
-    var val = e.value; // get the value. 
-    localStorage.setItem(id, val);// Every time user writing something, the localStorage's value will override . 
+    var id = e.id; 
+    var val = e.value; 
+    localStorage.setItem(id, val);
 }
 
-//get the saved value function - return the value of "v" from localStorage. 
 function getSavedValue  (v){
     if (!localStorage.getItem(v)) {
-        return "";// You can change this to your defualt value. 
+        return "";
     }
     return localStorage.getItem(v);
 }
@@ -68,4 +65,30 @@ function clearEP() {
   var propane = document.getElementById("propane");
   electric.value = "";
   propane.value = "";
+}
+
+function preset(number) {
+  if (number === 1) {
+    console.log("Preset " + number)
+    var input1 = document.getElementById("input1")
+    input1.value = "CCPF ";
+    var input2 = document.getElementById("input2")
+    input2.value = "E5 + CCPF ";
+    var input3 = document.getElementById("input3")
+    input3.value = "June Rent ";
+    var input4 = document.getElementById("input4")
+    input4.value = "June Rent + E5 + CCPF ";
+    var input5 = document.getElementById("input5")
+    input5.value = "June Rent + E5 + P5a + CCPF  ";
+    var input6 = document.getElementById("input6")
+    input6.value = "P5,a,b + CCPF ";
+    var input7 = document.getElementById("input7")
+    input7.value = "Monthly Auto Billing";
+    var input8 = document.getElementById("input8")
+    input8.value = "While processing your credit card for auto billing this month, your card did not go through. On the 6th of each month, late charges are applied. Please contact us with a card with sufficient funds or let us know when the card on your paper credit card authorization form can be charged. ";
+    var input9 = document.getElementById("input9")
+    input9.value = "";
+    var input10 = document.getElementById("input10")
+    input10.value = "";
+  }
 }
