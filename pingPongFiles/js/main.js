8051c26e2647c48184d11a7a57c3c5f6aceed8ae
lijahwhit <elijah@whitti.com>
2024-01-30 20:30:45 -0500

    temp ping pong

--- pingPongFiles/js/main.js
+++ pingPongFiles/js/main.js
@@ -7,17 +7,21 @@ let leftScore = document.querySelector('.leftScore');
 let rightScore = document.querySelector('.rightScore');
 let leftNumber = document.querySelector('.leftNumber');
 let rightNumber = document.querySelector('.rightNumber');
-let applyBtn = document.querySelector('#applyBtn');
 let resetBtn = document.querySelector('#resetBtn');
 let settingsView = document.querySelector('.settingsView');
 let notesView = document.querySelector('.notesView');
-// let leftAdjustments = document.querySelector('.leftAdjustments');
-// let rightAdjustments = document.querySelector('.rightAdjustments');
 let iconsBtns = document.querySelector('.iconsBtnsToggle');
 let SLBtn = document.querySelector('#startLeftBtn');
 let SRBtn = document.querySelector('#startRightBtn');
+let preset1 = document.querySelector('#preset1');
+let preset2 = document.querySelector('#preset2');
+let preset3 = document.querySelector('#preset3');
+let preset4 = document.querySelector('#preset4');
+
 leftNumber.innerText = 0;
 rightNumber.innerText = 0;
+preset1.style.backgroundColor = "green"
+leftNumber.style.color = "green"
 
 settingsView.classList.add('off');
 notesView.classList.add('off');
@@ -25,6 +29,7 @@ resetIcon.classList.add('off');
 
 let a = 0;
 let b = 0;
+let c = 0;
 let max = 99;
 let min = 0;
 let adj = 0;
@@ -34,38 +39,97 @@ let LIKeyV = 'ArrowUp';
 let LDKeyV = 'ArrowDown';
 let RIKeyV = 'ArrowRight';
 let RDKeyV = 'ArrowLeft';
+let L = 'green';
+let R = 'white'
 
+console.log("First L = " + L)
+console.log("First R = " + R)
 
-function leftScoreIncrease() {
-    if(a < max){
-        a++;
-        a = (a < max) ? a : a;
-        leftNumber.innerText = a;
+function scoreBoard(change) {
+    if (change === "lUp") {
+        if(a < max){
+            a++;
+            a = (a < max) ? a : a;
+            leftNumber.innerText = a;
+            };
+        }
+        
+        else if (change === "lDown") {
+            if(a > min){
+                a--;
+                a = (a < min) ? a : a;
+                leftNumber.innerText = a;
+            };
+            } 
+            
+            else if (change === "rUp") {
+                if(b < max){
+                    b++;
+                    b = (b < max) ? b : b;
+                    rightNumber.innerText = b;
+                };
+                } 
+                
+                else if (change === "rDown") {
+                    if(b > min){
+                        b--;
+                        b = (b < min) ? b : b;
+                        rightNumber.innerText = b;
+                    };
+                };
+    
+    if (change === "lUp" || change === "rUp") {
+        if ((a + b) % 2 === 0) {
+            if (L === 'green') {
+                rightNumber.style.color = L;
+                leftNumber.style.color = R;
+                L = 'white'
+                R = 'green'
+            } else if (L === 'white') {
+                rightNumber.style.color = L;
+                leftNumber.style.color = R;
+                L = 'green'
+                R = 'white'
+            };
+        };
+    } else if (change === "lDown" || change === "rDown") {
+        if ((a + b) % 2 === 0) {
+            if (L === 'white') {
+                rightNumber.style.color = R;
+                leftNumber.style.color = L;
+                L = 'white'
+                R = 'green'
+            } else if (L === 'green') {
+                rightNumber.style.color = R;
+                leftNumber.style.color = L;
+                L = 'green'
+                R = 'white'
+            };
+        };
     };
-};
+    console.log("L = " + L)
+    console.log("R = " + R)
 
-function leftScoreDecrease() {
-    if(a > min){
-        a--;
-        a = (a < min) ? a : a;
-        leftNumber.innerText = a;
-    };
-}
-
-function rightScoreIncrease() {
-    if(b < max){
-        b++;
-        b = (b < max) ? b : b;
-        rightNumber.innerText = b;
-    };
-};
-
-function rightScoreDecrease() {
-    if(b > min){
-        b--;
-        b = (b < min) ? b : b;
-        rightNumber.innerText = b;
-    };
+    //     if (change === "lUp" || change === "rUp") {
+//         if(c < max) {
+//             c = c + 1
+//         }
+//     } else if (change === "lDown" || change === "rDown") {
+//         if(c > min) {
+//             c = c - 1
+//         }
+//     };
+//     console.log(c)
+//     if (c % 2 === 0){
+//         rightNumber.style.color = "green";
+//         leftNumber.style.color = "white";
+//         console.log("Even")
+//     } else {
+//         rightNumber.style.color = "white";
+//         leftNumber.style.color = "green";
+//         console.log("odd")
+//     }
+// };
 };
 
 window.addEventListener("contextmenu", function (e) {
@@ -75,16 +139,15 @@ window.addEventListener("contextmenu", function (e) {
 window.addEventListener('keydown', (event) => {
     if (set === 0 && not === 0) {
         if (event.key === LIKeyV || event.key === 'AudioVolumeUp') {
-            leftScoreIncrease();
+            scoreBoard('lUp');
         }   else if (event.key === LDKeyV || event.key === 'AudioVolumeUp'){
-                leftScoreDecrease();
+                scoreBoard('lDown');
             }   else if (event.key === RIKeyV) {
-                    rightScoreIncrease();
+                    scoreBoard('rUp');
                 }   else if (event.key === RDKeyV) {
-                        rightScoreDecrease();
+                        scoreBoard('rDown');
                     };
-    }
-
+    };
 });
 
 
@@ -147,7 +210,6 @@ function settingsToggle() {
                     // console.log('1 - set OFF and adj ON')
                 };
     
-    deApply()
     deReset()
 };
 
@@ -211,65 +273,7 @@ function notesToggle() {
                     // console.log('1 - set OFF and adj ON')
                 };
 };
-
-// function adustmentBtnsToggle() {
-//     if (adj === 0) {
-//         // leftAdjustments.classList.add('on');
-//         // rightAdjustments.classList.add('on');
-//         body.classList.add('bodySmall')
-//         adj = 1
-//     }   else if (adj === 1){
-//         leftAdjustments.classList.remove('on');
-//         rightAdjustments.classList.remove('on');
-//         body.classList.remove('bodySmall')
-//         adj = 0
-//     };
-// };
-
-// function maxCount(){
-//     let maxNumber = document.getElementById('max').value;
-
-//     console.log(maxNumber)
-//     if (maxNumber == "" || maxNumber == null) {
-//         maxNumber = 99
-//     } else if (a > maxNumber || b > maxNumber) {
-//         reset()
-//         maxNumber = maxNumber
-//         } 
-
-//     max = maxNumber
-// }
-
-// function backColor() {
-//     let backColor = document.getElementById('backColor').value
-//     body.style.backgroundColor = backColor;
-// }
-
-function keyAssignment() {
-    let LIKey = document.getElementById('LIKey').value;
-    let LDKey = document.getElementById('LDKey').value;
-    let RIKey = document.getElementById('RIKey').value;
-    let RDKey = document.getElementById('RDKey').value;
-    LIKeyV = LIKey;
-    LDKeyV = LDKey;
-    RIKeyV = RIKey;
-    RDKeyV = RDKey;
-};
  
-function apply() {
-    // maxCount()
-    // backColor() 
-    keyAssignment()
-    
-    applyBtn.style.backgroundColor = "green";
-    applyBtn.value = "Applied";
-};
-
-function deApply() {
-    applyBtn.style.backgroundColor = "#000000";
-    applyBtn.value = "Apply";
-}
-
 function reset() {
     leftNumber.innerText = 0;
     rightNumber.innerText = 0;
@@ -281,62 +285,91 @@ function reset() {
     rightNumber.style.color = "white"
     SLBtn.style.backgroundColor = "black";
     SRBtn.style.backgroundColor = "black";
-}
+};
 
 function deReset() {
     resetBtn.style.backgroundColor = "#000000";
     resetBtn.value = "Reset";
-}
+};
 
 function preset(number) {
-    deApply()
     if (number === 1) {
-        document.getElementById('LIKey').value = 'AudioVolumeUp';
-        document.getElementById('LDKey').value = 'AudioVolumeDown';
-        document.getElementById('RIKey').value = 'ArrowUp';
-        document.getElementById('RDKey').value = 'ArrowDown';
+        document.getElementById('LIKey').value = "ArrowUp" ;
+        document.getElementById('LDKey').value = "ArrowDown";
+        document.getElementById('RIKey').value = "ArrowRight";
+        document.getElementById('RDKey').value = "ArrowLeft";
+        preset1.style.backgroundColor = "green";
+        preset2.style.backgroundColor = "black";
+        preset3.style.backgroundColor = "black";
+        preset4.style.backgroundColor = "black";
         } else if (number === 2) {
             document.getElementById('LIKey').value = 7;
             document.getElementById('LDKey').value = 4;
             document.getElementById('RIKey').value = 9;
             document.getElementById('RDKey').value = 6;
+            preset1.style.backgroundColor = "black";
+            preset2.style.backgroundColor = "green";
+            preset3.style.backgroundColor = "black";
+            preset4.style.backgroundColor = "black";
             } else if (number === 3) {
                 document.getElementById('LIKey').value = 'w';
                 document.getElementById('LDKey').value = 's';
                 document.getElementById('RIKey').value = 'o';
                 document.getElementById('RDKey').value = 'l';
-                } else {
-                    document.getElementById('LIKey').value = "ArrowUp" ;
-                    document.getElementById('LDKey').value = "ArrowDown";
-                    document.getElementById('RIKey').value = "ArrowRight";
-                    document.getElementById('RDKey').value = "ArrowLeft";
+                preset1.style.backgroundColor = "black";
+                preset2.style.backgroundColor = "black";
+                preset3.style.backgroundColor = "green";
+                preset4.style.backgroundColor = "black";
+                } else if (number === 4) {
+                    document.getElementById('LIKey').value = 'AudioVolumeUp';
+                    document.getElementById('LDKey').value = 'AudioVolumeDown';
+                    document.getElementById('RIKey').value = 'ArrowUp';
+                    document.getElementById('RDKey').value = 'ArrowDown';
+                    preset1.style.backgroundColor = "black";
+                    preset2.style.backgroundColor = "black";
+                    preset3.style.backgroundColor = "black";
+                    preset4.style.backgroundColor = "green";
                 }
-}
+    let LIKey = document.getElementById('LIKey').value;
+    let LDKey = document.getElementById('LDKey').value;
+    let RIKey = document.getElementById('RIKey').value;
+    let RDKey = document.getElementById('RDKey').value;
+    LIKeyV = LIKey;
+    LDKeyV = LDKey;
+    RIKeyV = RIKey;
+    RDKeyV = RDKey;
+};
 
 function blank() {
     if (set == 1) {
         settingsToggle()
     } else if (not === 1) {
         notesToggle()
-    }
-}
+    };
+};
 
 function iconsBtnsToggle() {
     settingsIcon.classList.toggle('off');
     notesIcon.classList.toggle('off');
-    // adjustmentBtnsIcon.classList.toggle('off');
-}
+};
 
 function startServerBtn(button) {
-    if (button === 1) {
-        SLBtn.style.backgroundColor = "black";
-        SRBtn.style.backgroundColor = "green";
-        rightNumber.style.color = "green"
-        leftNumber.style.color = "white"
-    } else if (button === 0) {
+    if (button === 'left') {
         SLBtn.style.backgroundColor = "green";
         SRBtn.style.backgroundColor = "black";
         rightNumber.style.color = "white"
         leftNumber.style.color = "green"
-    }
-}
+        L = 'green'
+        R = 'white'
+    } else if (button === 'right') {
+        SLBtn.style.backgroundColor = "black";
+        SRBtn.style.backgroundColor = "green";
+        rightNumber.style.color = "green"
+        leftNumber.style.color = "white"
+        L = 'white'
+        R = 'green'
+    } 
+};
+
+
+
